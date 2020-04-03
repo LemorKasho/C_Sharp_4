da2f2a59eb652de91449f32fa94ed367bb9a9da2
lemorkasho04 <lemorkasho04@gmail.com>
2020-04-03 21:45:21 +0300

    Add project files.

---
+++ C_Sharp_4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace C_Sharp_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserAnswer;
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Please Enter Your Number");
+                float Number1 = Convert.ToSingle(Console.ReadLine());
+                float Result = Number1;
+
+            OperationAgain:
+                Console.WriteLine("Please Enter Your  Operation : + , - , * , /");
+                char Operation = char.Parse(Console.ReadLine());
+
+                float Number;
+                switch (Operation)
+                {
+                    case '+':
+                        Console.WriteLine("Please Enter Your Number");
+                        Number = Convert.ToSingle(Console.ReadLine());
+                        Result += Number;
+                        goto OperationAgain;
+
+                    case '-':
+                        Console.WriteLine("Please Enter Your Number");
+                        Number = Convert.ToSingle(Console.ReadLine());
+                        Result -= Number;
+                        goto OperationAgain;
+
+                    case '*':
+                        Console.WriteLine("Please Enter Your Number");
+                        Number = Convert.ToSingle(Console.ReadLine());
+                        Result *= Number;
+                        goto OperationAgain;
+
+                    case '/':
+                        Console.WriteLine("Please Enter Your Number");
+                        Number = Convert.ToSingle(Console.ReadLine());
+                        Result /= Number;
+                        goto OperationAgain;
+
+                    case '=':
+                        Console.WriteLine("Result = {0}", Result);
+                        break;
+                    default:
+                        goto OperationAgain;
+                }
+                do
+                {
+                    Console.WriteLine("Do You Want Try Again ?? YES or NO");
+                    UserAnswer = Console.ReadLine().ToUpper();
+
+                } while (UserAnswer != "YES" && UserAnswer != "NO");
+            } while (UserAnswer == "YES");
+            Console.Clear();
+            Console.WriteLine("Welcom, Byee Byee");
+        }
+    }
+}
