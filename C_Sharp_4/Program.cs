f17b54efd0b1b78e8d7d3e769da1f14016c05635
lemorkasho04 <lemorkasho04@gmail.com>
2020-04-28 17:37:45 +0300

    Edit On Calculator

--- C_Sharp_4/Program.cs
+++ C_Sharp_4/Program.cs
@@ -6,58 +6,77 @@ namespace C_Sharp_4
         static void Main(string[] args)
         {
             string UserAnswer;
+            char Operation;
             do
             {
-                Console.Clear();
-
                 Console.WriteLine("Please Enter Your Number");
                 float Number1 = Convert.ToSingle(Console.ReadLine());
                 float Result = Number1;
-
-            OperationAgain:
-                Console.WriteLine("Please Enter Your  Operation : + , - , * , /");
-                char Operation = char.Parse(Console.ReadLine());
-
-                float Number;
-                switch (Operation)
+                do
                 {
-                    case '+':
-                        Console.WriteLine("Please Enter Your Number");
-                        Number = Convert.ToSingle(Console.ReadLine());
-                        Result += Number;
-                        goto OperationAgain;
 
-                    case '-':
-                        Console.WriteLine("Please Enter Your Number");
-                        Number = Convert.ToSingle(Console.ReadLine());
-                        Result -= Number;
-                        goto OperationAgain;
+                    Console.WriteLine("Please Enter Your Operation : + , - , * , /");
+                    Operation = char.Parse(Console.ReadLine());
 
-                    case '*':
-                        Console.WriteLine("Please Enter Your Number");
-                        Number = Convert.ToSingle(Console.ReadLine());
-                        Result *= Number;
-                        goto OperationAgain;
+                    float Number;
+                    switch (Operation)
+                    {
 
-                    case '/':
-                        Console.WriteLine("Please Enter Your Number");
-                        Number = Convert.ToSingle(Console.ReadLine());
-                        Result /= Number;
-                        goto OperationAgain;
+                        case '+':
+                            Console.WriteLine("Please Enter Your Number");
+                            Number = Convert.ToSingle(Console.ReadLine());
+                            Result += Number;
+                            break;
 
-                    case '=':
-                        Console.WriteLine("Result = {0}", Result);
-                        break;
-                    default:
-                        goto OperationAgain;
-                }
+                        case '-':
+                            Console.WriteLine("Please Enter Your Number");
+                            Number = Convert.ToSingle(Console.ReadLine());
+                            Result -= Number;
+                            break;
+
+                        case '*':
+                            Console.WriteLine("Please Enter Your Number");
+                            Number = Convert.ToSingle(Console.ReadLine());
+                            Result *= Number;
+                            break;
+
+                        case '/':
+                            do
+                            {
+                                Console.WriteLine("Please Enter Your Number");
+                                Number = Convert.ToSingle(Console.ReadLine());
+                                if (Number != 0)
+                                {
+                                    Result /= Number;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Cannot divide by zero");
+                                }
+                            } while (Number == 0);
+                            break;
+
+                        case '=':
+                            Console.WriteLine("Result = {0}", Result);
+                            break;
+
+                        default:
+                            Console.WriteLine("Uncorrected Opertion {0}", Operation);
+                            break;
+                    }
+                } while (Operation == '+' || Operation == '-' || Operation == '*' || Operation == '/' || Operation != '=');
+
                 do
                 {
                     Console.WriteLine("Do You Want Try Again ?? YES or NO");
                     UserAnswer = Console.ReadLine().ToUpper();
 
                 } while (UserAnswer != "YES" && UserAnswer != "NO");
+
+                Console.Clear();
+
             } while (UserAnswer == "YES");
+
             Console.Clear();
             Console.WriteLine("Welcom, Byee Byee");
         }
